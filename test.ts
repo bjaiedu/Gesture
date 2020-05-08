5451b30a02ca2cf75ae0510135ec65b232c9d89e
bjaiedu <36872424+bjaiedu@users.noreply.github.com>
2020-05-08 17:10:47 +0800

    Add files via upload

--- test.ts
+++ test.ts
@@ -1,1 +1,18 @@
-// tests go here; this will not be compiled when this package is used as a library
+let apds = new GestureSensor.apds9960;
+if (!(apds.begin(10, GestureSensor.apds9960AGain_t.APDS9960_AGAIN_4X, GestureSensor.APDS9960_I2C_ADDR))) {
+    basic.showIcon(IconNames.No);
+} else {
+    basic.showIcon(IconNames.Yes);
+}
+basic.pause(1000);
+apds.enableProximity(1);
+apds.enableGesture(1);
+basic.forever(function () {
+    let gesturevalu = apds.readGesture();
+    basic.showNumber(apds.value1);
+    basic.pause(1000);
+    basic.showNumber(apds.value2);
+    basic.pause(1000);
+    basic.showNumber(apds.value3);
+    basic.pause(1000)
+})
