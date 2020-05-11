253c3a0ddf0640c101ea518b71f9bc5c56d76b3b
bjaiedu <36872424+bjaiedu@users.noreply.github.com>
2020-05-11 10:47:38 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,84 +1,54 @@
-// % weight=10 color=#9F79EE icon="\uf108" block="×ËÊÆ´«¸ÐÆ÷"
+// % weight=10 color=#9F79EE icon="\uf108" block="æ‰‹åŠ¿ä¼ æ„Ÿå™¨"
 namespace mytest{
-
-    enum Direction_type{
-
-        DIR_NONE = 0,
-
+    enum Direction_type {
+        //% block = None
+        DIR_NONE = 0,        
+        //% block = UP
         DIR_UP = 1,
-
+        //% block = DOWN
         DIR_DOWN = 2,
-
+        //% block = LEFT
         DIR_LEFT = 3,
-
-        DIR_RIGHT = 4
-
+        //% block = RIGHT
+        DIR_RIGHT = 4,
+        //% block = NEAR
+        DIR_NEAR = 5,
+        //% block = FAR
+        DIR_FAR = 6,
+        //% block = ALL
+        DIR_ALL = 7
     }
 
     export class apds9960{
-
         begin(x:number,y:number,z:number){
-
             return true
-
         }
-
-
-
         enableProximity(x:number){}
-
         enableGesture(x:number){}
-
         readGesture(){return Direction_type.DIR_LEFT}
-
     }
-
+    
     const gestureEventId = 4100;
-
     let lastGetureValue = Direction_type.DIR_NONE;
-
     let apds = new apds9960();
 
-
-
     //% blockId="GET_GESTURE_VALUE" block="Gesture|%gesture"
-
     //% weight=100 color=#000012
-
-
-
     export function onGesture(gesture: Direction_type, handler: () => void) {
-
         control.onEvent(gestureEventId, gesture, handler);
-
         if (apds.begin(10, 0x01, 0x39)) {
-
             apds.enableProximity(1);
-
             apds.enableGesture(1);
-
         } else {
-
             basic.showIcon(IconNames.No)
-
         }
-
         control.inBackground(() => {
-
             const gestureValue = apds.readGesture();
-
             if (gestureValue != lastGetureValue) {
-
                 lastGetureValue = gestureValue;
-
                 control.raiseEvent(gestureEventId, lastGetureValue)
-
             }
-
             basic.pause(10)
-
         })
-
     }
-
 }
